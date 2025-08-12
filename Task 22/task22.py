a8c509564d6047be2999a7d26a157be23e6bcce2
maxchen12345 <64642333+maxchen12345@users.noreply.github.com>
2025-08-12 14:52:51 +1200

    Add files via upload

---
+++ Task 22/task22.py
@@ -0,0 +1,11 @@
+print("Balance of the account is $ 100")
+Balance = 100
+short = 1.79 * 3
+medium = 2.5 * 4
+long = 4.5 * 2
+short = float(short)
+medium = float(medium)
+long = float(long)
+total = Balance - (short + medium + long)
+print("You balance after is $",total)
+print("You have spent $", short + medium + long)
