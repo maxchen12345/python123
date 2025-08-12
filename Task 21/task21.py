e04bf6b91b35d006831001e361072c81c90256c0
maxchen12345 <64642333+maxchen12345@users.noreply.github.com>
2025-08-12 14:52:17 +1200

    Add files via upload

---
+++ Task 21/task21.py
@@ -0,0 +1,18 @@
+print("inital prices")
+diesel = 1.47
+octane = 2.17
+unleaded = 2.06
+print("diesel price", diesel)
+print("unleaded price", unleaded)
+print("octane price", octane)
+# Price Increase
+unleadedi = unleaded * 1.10
+dieseli = diesel * 1.10
+octanei = octane * 1.10
+unleadedo = round(unleadedi, 2)
+dieselo = round(dieseli, 2)
+octaneo = round(octanei, 2)
+print("")
+print("Diesel", dieselo)
+print("Unleaded", unleadedo)
+print("Octane", octaneo)
