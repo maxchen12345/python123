8477a3de8d10ed4b44e171ed9e0a1af1bd29fc05
maxchen12345 <64642333+maxchen12345@users.noreply.github.com>
2025-08-12 14:31:34 +1200

    Add files via upload

---
+++ Task 9/task9.py
@@ -0,0 +1,8 @@
+pi = 3.14159265358979323
+radius = input("what is the radius of the circle? ")
+pi = float(pi)
+radius = float(radius)
+sum1 = radius ** 2
+sum1 = float(sum1)
+sum = sum1 * pi
+print(sum,"is the area")
