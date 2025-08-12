dd7baf9d9ee178270067fd148de728a88ac1221e
maxchen12345 <64642333+maxchen12345@users.noreply.github.com>
2025-08-12 15:14:32 +1200

    Add files via upload

---
+++ Task 23/task23.py
@@ -0,0 +1,9 @@
+punnet = 3.5
+box = 19.5
+bucket = 63
+punnet = float(punnet)
+box = float(box)
+bucket = float(bucket)
+total = punnet + box + bucket
+total_price = total * 5
+print("Total cost of the fruits is $", total_price)
